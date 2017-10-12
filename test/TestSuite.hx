ddc286f35c36c84d55c7698f7451efed491ea446
nightblade9 <nightbladecodes@gmail.com>
2017-10-12 12:47:01 -0400

    Hello World flixel template 800x450 resolution!

--- test/TestSuite.hx
+++ test/TestSuite.hx
@@ -2,9 +2,9 @@ import massive.munit.TestSuite;
 
 import emanadventures.generators.StoryGeneratorTest;
 import emanadventures.model.ArtifactTest;
+import emanadventures.model.WorldTest;
 import emanadventures.model.templates.EventTemplateTest;
 import emanadventures.model.templates.StoryTemplateTest;
-import emanadventures.model.WorldTest;
 
 /**
  * Auto generated Test Suite for MassiveUnit.
@@ -20,8 +20,8 @@ class TestSuite extends massive.munit.TestSuite
 
 		add(emanadventures.generators.StoryGeneratorTest);
 		add(emanadventures.model.ArtifactTest);
+		add(emanadventures.model.WorldTest);
 		add(emanadventures.model.templates.EventTemplateTest);
 		add(emanadventures.model.templates.StoryTemplateTest);
-		add(emanadventures.model.WorldTest);
 	}
 }
