51b0639cc6f00738e9b091a99889f1d5ecaca26c
nightblade9 <nightbladecodes@gmail.com>
2017-10-15 10:52:37 -0400

    Player moves to WASD. View only, no model/tests.

--- src/emanadventures/states/HouseState.hx
+++ src/emanadventures/states/HouseState.hx
@@ -2,6 +2,7 @@ package emanadventures.states;
 
 import flixel.util.FlxColor;
 import helix.core.HelixState;
+import helix.core.HelixSprite;
 
 class HouseState extends HelixState
 {
@@ -9,6 +10,8 @@ class HouseState extends HelixState
 	{
 		super.create();
 		this.bgColor = FlxColor.BROWN;
+
+		var player = new HelixSprite("assets/images/player.png").moveWithKeyboard(100);
 	}
 
 	override public function update(elapsedSeconds:Float):Void
