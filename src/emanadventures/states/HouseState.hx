e1ef7e6b8049f577ec124c3b132918ebcbc174a4
nightblade9 <nightbladecodes@gmail.com>
2017-10-16 00:18:42 -0400

    Player collides against walls.

--- src/emanadventures/states/HouseState.hx
+++ src/emanadventures/states/HouseState.hx
@@ -11,7 +11,25 @@ class HouseState extends HelixState
 		super.create();
 		this.bgColor = FlxColor.BROWN;
 
-		var player = new HelixSprite("assets/images/player.png").moveWithKeyboard(100);
+		var player = new HelixSprite("assets/images/player.png").moveWithKeyboard(100).move(200, 200);
+		var text = openfl.Assets.getText("assets/data/home.json");
+
+		// Remove comments
+        var regex = new EReg("//.*", "g");
+		text = regex.replace(text, "");
+
+		var json = haxe.Json.parse(text);
+		var wallSizes:Array<Array<Int>> = json.walls;
+
+		for (wallSize in wallSizes)
+		{
+			var x = wallSize[0];
+			var y = wallSize[1];
+			var orientation = wallSize[2];
+
+			var wall = new HelixSprite('assets/images/wall-${orientation}.png').collisionImmovable().move(x, y);
+			player.collideResolve(wall);
+		}
 	}
 
 	override public function update(elapsedSeconds:Float):Void
