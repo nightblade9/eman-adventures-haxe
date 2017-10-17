8a02d6eeb63e0538c290a15371386316beaed9e5
nightblade9 <nightbladecodes@gmail.com>
2017-10-16 23:53:33 -0400

    Add exit detection. No sprite. Rename HouseState => HomeState

--- Main.hx
+++ Main.hx
@@ -8,6 +8,6 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(0, 0, emanadventures.states.HouseState, 1, 60, 60, true));
+		addChild(new FlxGame(0, 0, emanadventures.states.HomeState, 1, 60, 60, true));
 	}
 }
--- src/emanadventures/states/HomeState.hx
+++ src/emanadventures/states/HomeState.hx
@@ -5,14 +5,19 @@ import flixel.util.FlxColor;
 import helix.core.HelixState;
 import helix.core.HelixSprite;
 
-class HouseState extends HelixState
+class HomeState extends HelixState
 {
+	private var player:HelixSprite;
+	private var exit:Dynamic; // JSON
+
 	override public function create():Void
 	{
 		super.create();
 		this.bgColor = FlxColor.BROWN;
 
-		var player = new HelixSprite("assets/images/player.png").moveWithKeyboard(100).move(200, 200);
+		this.player = new HelixSprite("assets/images/player.png").moveWithKeyboard(100);
+		// Start in the middle of the room
+		player.move((this.width - player.width) / 2, (this.height - player.height) / 2);
 		var json = JsonReader.readJsonAsset("assets/data/home.json");
 		var wallSizes:Array<Array<Int>> = json.walls;
 
@@ -25,10 +30,19 @@ class HouseState extends HelixState
 			var wall = new HelixSprite('assets/images/wall-${orientation}.png').collisionImmovable().move(x, y);
 			player.collideResolve(wall);
 		}
+
+		this.exit = json.exit;
 	}
 
 	override public function update(elapsedSeconds:Float):Void
 	{
 		super.update(elapsedSeconds);
+
+		// Exit is not a real sprite. It's just a location. AABB collision check.
+		if (player.x >= exit.x && player.x <= exit.x + exit.width && 
+			player.y >= exit.y && player.y <= exit.y + exit.height)
+		{
+			trace(Date.now());	
+		}
 	}
 }
