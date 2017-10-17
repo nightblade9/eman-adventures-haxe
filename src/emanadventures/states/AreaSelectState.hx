e65bd375b55ab086e72485f95740d7e49947f9ec
nightblade9 <nightbladecodes@gmail.com>
2017-10-17 08:34:45 -0400

    Area select state with placeholder icons

--- src/emanadventures/states/AreaSelectState.hx
+++ src/emanadventures/states/AreaSelectState.hx
@@ -6,9 +6,22 @@ import helix.core.HelixSprite;
 
 class AreaSelectState extends HelixState
 {
+    private static inline var PADDING:Int = 64;
+    private var knownAreas = ["Silver Forest", "Ice Temple", "Cave"];
+
 	override public function create():Void
 	{
         this.bgColor = FlxColor.BLACK;
+
+        for (i in 0 ... knownAreas.length)
+        {
+            var x = PADDING + ((i + 1) * 2 * PADDING);
+
+            new HelixSprite("assets/images/area-icon.png").move(x, 150)
+            .onClick(function() {
+                trace("Clicked on " + i);
+            });
+        }
 	}
 
 	override public function update(elapsedSeconds:Float):Void
